604bd9ac97a226502d3a6ab0a43b311712dfa11d
josé jonathan <josejonathan689@gmail.com>
2021-06-13 17:03:45 -0300

    conclusão da primeira parte tanto nos detalhes de estilização quanto nas funcionalidades JavaScript

--- script.js
+++ script.js
@@ -7,6 +7,156 @@ const Storage = {
     }
 }
 
+const date = new Date()
+
+const Transaction = {
+    all: Storage.get(),
+    addTransaction(newData) {
+        Transaction.all.push(newData)
+
+        App.reload()
+    },
+    removeTransaction() {
+        Transaction.all.splice(Transaction.all)
+
+        App.reload()
+    },
+}
+
+const DOM = {
+    transactionContainer: document.querySelector('tbody'),
+    insertHTML(transaction, index) {
+
+        const html = `
+        <td>${index+1}</td>
+        <td class="image-table">${transaction.cardNumber}</td>
+        <td>${Utils.formatDate(date)}</td>
+        <td>${transaction.securityCode} / ${transaction.expiryDate} Months</td>
+        <td class="image-table">${transaction.paymentType}</td>
+        <td class="completed-transaction">${transaction.username}</td>
+        `;
+
+        return html
+    },
+    returnData(transaction, index) {
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = DOM.insertHTML(transaction, index)
+        tr.dataset.index = index
+
+        DOM.transactionContainer.appendChild(tr)
+
+        return DOM.transactionContainer
+    },
+    clearData() {
+        DOM.transactionContainer.innerHTML = ""
+    }
+}
+
+const Form = {
+    paymentType: document.getElementsByName('payment-mode-input'),
+    cardNumber: document.querySelector('div input#card-number'),
+    userName: document.querySelector('input#name'),
+    expiryDate: document.querySelector('input#date'),
+    securityCode: document.querySelector('input#security-code'),
+    getValues() {
+        let paymentType;
+
+        for(let i=0; i< Form.paymentType.length; i++){
+            if(Form.paymentType[i].checked){
+                paymentType = Form.paymentType[i].value
+                break;
+            }
+        }
+
+        return {
+            paymentType: paymentType,
+            cardNumber: Form.cardNumber.value,
+            username: Form.userName.value,
+            expiryDate: Form.expiryDate.value,
+            securityCode: Form.securityCode.value
+        }
+    },
+    validateData() {
+        const { paymentType, cardNumber, username, expiryDate, securityCode } = Form.getValues()
+
+        if (paymentType == "" || cardNumber.trim() == "" || username.trim() === ""
+            || expiryDate.trim() == "" || securityCode.trim() == "") {
+
+            throw new Error("Por favor preencha todos os campos para continuar!")
+        }
+
+    },
+    formatData() {
+        let { paymentType, cardNumber, username, expiryDate, securityCode } = Form.getValues()
+        let expiryDates;
+        let cardNumbers;
+        let paymentTypes;
+        let securityCodes;
+        let userName = String(username);
+
+        if (paymentType == "Paypal") {
+
+            paymentTypes = Utils.forPaypal()
+
+        } else if (paymentType == "Card") {
+
+            paymentTypes = Utils.forCard()
+
+        }
+
+        if (cardNumber.length >= 16) {
+
+            cardNumbers = Utils.forStartup()
+
+        } else if (cardNumber.length < 16 && cardNumber.length >= 14) {
+
+            cardNumbers = Utils.forBusiness()
+
+        } else if (cardNumber.length < 14) {
+
+            cardNumbers = Utils.forEconomy()
+
+        }
+
+        expiryDates = Utils.dateDifference(expiryDate, date)
+
+        securityCodes = Utils.toCash(securityCode, date)
+
+        return {
+            paymentType: paymentTypes,
+            cardNumber: cardNumbers,
+            username: userName,
+            expiryDate: expiryDates,
+            securityCode: securityCodes
+        }
+    },
+    saveData(newData) {
+        Transaction.addTransaction(newData)
+    },
+    clearData() {
+        Form.paymentType.value = "",
+        Form.cardNumber.value = "",
+        Form.userName.value = "",
+        Form.expiryDate.value = "",
+        Form.securityCode.value = ""
+    },
+    submit(event) {
+        event.preventDefault();
+
+        try {
+            Form.getValues();
+            Form.validateData();
+            const data = Form.formatData();
+            Form.saveData(data);
+            Form.clearData();
+        } catch (error) {
+            alert(error.message)
+        }
+
+    }
+}
+
 
 const Utils = {
     forPaypal() {
@@ -34,16 +184,16 @@ const Utils = {
 
         return tdContentEconomy
     },
-    formatDate() {
-        const date = new Date()
+    formatDate(dates) {
+        const date = dates;
         const oldDate = String(date).split(" ")
 
         let finalDate = `${oldDate[2]}/${oldDate[1]}/${oldDate[3]}`
 
         return finalDate
     },
-    dateDifference(expiryDates) {
-        const currentDate = new Date()
+    dateDifference(expiryDates,dates) {
+        const currentDate = dates
         let expiryDate = expiryDates
         const currentMonth = String(currentDate).split(" ")
         let expiryDateShare = String(expiryDate).split("-")
@@ -89,10 +239,38 @@ const Utils = {
                 break;
         }
 
+        let differenceYears = currentMonth[3] - expiryDateShare[0]
         let differenceMonths = currentMonth[1] - expiryDateShare[1]
+        let MonthsforYears;
+
+        if(differenceYears != 0){
+            switch (differenceYears) {
+                case -1:
+                    MonthsforYears = (12 - currentMonth[1]) + Number(expiryDateShare[1]);
+                    break;
+                case -2:
+                    MonthsforYears = (12- currentMonth[1]) + 12 + Number(expiryDateShare[1]);
+                    break;
+                case -3:
+                    MonthsforYears = (12- currentMonth[1]) + 24 + Number(expiryDateShare[1]);
+                    break;
+                case -4:
+                    MonthsforYears = (12- currentMonth[1]) + 36 + Number(expiryDateShare[1]);
+                    break;
+                case -5:
+                    MonthsforYears = (12- currentMonth[1]) + 48 + Number(expiryDateShare[1]);
+                    break;
+                                
+                default:
+                    break;
+            }
+
+            differenceMonths = MonthsforYears;
+        }
+
+
         let result = String(differenceMonths).replace("-", "")
 
-        console.log(differenceMonths)
         return result
     },
     toCash(cvv) {
@@ -105,148 +283,6 @@ const Utils = {
         return money
     }
 }
-
-const Transaction = {
-    all: Storage.get(),
-    addTransaction(newData) {
-        Transaction.all.push(newData)
-
-        App.reload()
-    },
-    removeTransaction(index) {
-        Transaction.all.splice(index, 1)
-
-        App.reload()
-    },
-}
-
-const DOM = {
-    transactionContainer: document.querySelector('tbody'),
-    insertHTML(transaction, index) {
-
-        const html = `
-        <td>${index}</td>
-        <td class="image-table">${transaction.cardNumber}</td>
-        <td>${Utils.formatDate()}</td>
-        <td>${transaction.securityCode} / ${transaction.expiryDate} Months</td>
-        <td class="image-table">${transaction.paymentType}</td>
-        <td class="completed-transaction">Completed</td>
-        `;
-
-        return html
-    },
-    returnData(transaction, index) {
-        const tr = document.createElement('tr')
-
-        tr.innerHTML = DOM.insertHTML(transaction, index)
-        tr.dataset.index = index
-
-        DOM.transactionContainer.appendChild(tr)
-
-        return DOM.transactionContainer
-    },
-    clearData() {
-        DOM.transactionContainer.innerHTML = ""
-    }
-}
-
-const Form = {
-    paymentType: document.querySelector('div.payment-paypal-card-mode input'),
-    cardNumber: document.querySelector('div input#card-number'),
-    userName: document.querySelector('input#name'),
-    expiryDate: document.querySelector('input#date'),
-    securityCode: document.querySelector('input#security-code'),
-    getValues() {
-        return {
-            paymentType: Form.paymentType.value,
-            cardNumber: Form.cardNumber.value,
-            username: Form.userName.value,
-            expiryDate: Form.expiryDate.value,
-            securityCode: Form.securityCode.value
-        }
-    },
-    validateData() {
-        const { paymentType, cardNumber, username, expiryDate, securityCode } = Form.getValues()
-
-        if (paymentType.trim() == "" || cardNumber.trim() == "" || username.trim() === ""
-            || expiryDate.trim() == "" || securityCode.trim() == "") {
-
-            throw new Error("Por favor preencha todos os campos para continuar!")
-        }
-
-    },
-    formatData() {
-        let { paymentType, cardNumber, username, expiryDate, securityCode } = Form.getValues()
-        let expiryDates;
-        let cardNumbers;
-        let paymentTypes;
-        let securityCodes;
-
-        console.log(expiryDate)
-
-        if (paymentType.trim() == "Paypal") {
-
-            paymentTypes = Utils.forPaypal()
-
-        } else if (paymentType.trim() == "Card") {
-
-            paymentTypes = Utils.forCard()
-
-        }
-
-        if (cardNumber.length >= 16) {
-
-            cardNumbers = Utils.forStartup()
-
-        } else if (cardNumber.length < 16 && cardNumber.length >= 14) {
-
-            cardNumbers = Utils.forBusiness()
-
-        } else if (cardNumber.length < 14) {
-
-            cardNumbers = Utils.forEconomy()
-
-        }
-
-        expiryDates = Utils.dateDifference(expiryDate)
-
-        securityCodes = Utils.toCash(securityCode)
-
-        return {
-            paymentType: paymentTypes,
-            cardNumber: cardNumbers,
-            username: username,
-            expiryDate: expiryDates,
-            securityCode: securityCodes
-        }
-    },
-    saveData(newData) {
-        Transaction.addTransaction(newData)
-    },
-    clearData() {
-        Form.paymentType.value = "",
-            Form.cardNumber.value = "",
-            Form.username.value = "",
-            Form.expiryDate.value = "",
-            Form.securityCode.value = ""
-    },
-    submit(event) {
-        event.preventDefault();
-
-        try {
-            Form.getValues();
-            Form.validateData();
-            const data = Form.formatData();
-            Form.saveData(data);
-            Form.clearData();
-
-        } catch (error) {
-            alert(error.message)
-        }
-
-    }
-}
-
 
 const App = {
     init() {
